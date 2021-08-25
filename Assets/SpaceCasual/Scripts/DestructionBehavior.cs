f2a66c11068a573dd0dad3ab434f551441478a8a
Zach-0313 <72165465+Zach-0313@users.noreply.github.com>
2021-08-25 08:40:38 -0400

    Destruction Behavior Started
    Next, create + test DestructionProfile ScriptableObject

---
+++ Assets/SpaceCasual/Scripts/DestructionBehavior.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructionBehavior : MonoBehaviour
+{
+    public DestructionProfile profile;  //Destruction Profile will be a scriptableObject, holding damage models, particles, and data.
+    float FuelAdd;
+    float ScoreAdd;
+    bool Destroyed;
+
+    ParticleSystem ContactEffect;
+    ParticleSystem RubbleEffect;
+
+    void Start()
+    {
+        FuelAdd = profile.Fuel;
+        ScoreAdd = profile.Score;
+    }
+    public void ApplyContact(Vector3 contactPoint)
+    {
+        ParticleSystem contact = Instantiate(ContactEffect, contactPoint, Quaternion.identity);
+        contact.Play();
+    }
+    public void Crumble()
+    {
+        Instantiate(profile.Destroyed, transform.position);
+        Instantiate(RubbleEffect, transform.position, Quaternion.identity);
+    }
+}
+
